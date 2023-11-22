ee691f2a09df60b74937948a97b72488a5ff7c18
Sharmasumesh <sharmasumesh28@gmail.com>
2023-11-22 12:44:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { useState,useEffect } from "react";
+import "./CSS/Home.css"
+function App()
+{
+  
+  
+  const [city,setcity]=useState(null);
+  const [search,setsearch] = useState("delhi");
+  useEffect(()=>{
+    const fetchApi  = async()=>{
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=37941ef580eff211a7045e9c1d28414d)`
+      const response = await fetch(url);
+      // console.log(response);
+      const resjson = response.json();
+      console.log(resjson);
+      setcity(resjson )
+    }
+    fetchApi();
+  })
+  return(
+    <>
+    <div className="box"></div>
+    <input type="text" placeholder="Enter the name" onChange={(event)=>{
+      setsearch(event.target.value)
+    }}/>
+    <button>Check</button>
+    <div className="data">
+      <h1>{search}</h1>
+      <h2>temp</h2>
+      <span>min</span>|<span>max</span>
+      <h6>humdity</h6>
+      <h6>Air pressure</h6>
     </div>
-  );
+    </>
+  )
 }
-
 export default App;
